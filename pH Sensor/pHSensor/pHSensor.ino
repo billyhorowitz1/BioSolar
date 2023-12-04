db56dde0fe79f8b3bc361f1fd66034f6ba6a3674
billy_horowitz1 <whorowitz2@gmail.com>
2023-12-04 13:59:06 -0500

    Set up code to be used for calibraring sensor

--- pH Sensor/pHSensor/pHSensor.ino
+++ pH Sensor/pHSensor/pHSensor.ino
@@ -1,9 +1,22 @@
+// Configuration Variables
+
+int sensorPin = A0; // Establish A0 as the pin that the pH sensor hooks up to
+
+// Setup Function
 void setup() {
-  // put your setup code here, to run once:
+ 
+    Serial.begin(9600); // Start serial communication at baud rate of 9600
 
 }
 
+// Loop Function
 void loop() {
-  // put your main code here, to run repeatedly:
+  
+    int sensorValue = analogRead(A0); // Read analog value from pH sensor
+    float voltageValue = sensorValue * (5.0/1023.0); // Converting analog values to voltage values for calibration
+    
+    Serial.println(voltageValue); // Print out the voltage value to serial monitor
+
+    delay(1000); // Print every second
 
 }
